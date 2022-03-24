6fc1c7c8305d02cf9e7ebd63c9a285589e1d15bb
frlsua <lera20053003@gmail.com>
2022-03-24 18:07:28 +0300

    lesson 3

---
+++ project/script.js
@@ -0,0 +1,19 @@
+//alert("Hello JS");
+
+//alert(nn);
+
+function guess() {
+    var nn = prompt("Введите число");
+    if (nn > 5) {
+        alert("Число должно быть меньше");
+        return guess();
+    } else if (nn == 5) {
+        return alert("Угадали");
+        
+    } else {
+        alert("Число должно быть больше");
+        return guess();
+    }
+}
+
+guess();
