7bfef98d72a5bbacd319e8d382c6826c25692fd2
frlsua <lera20053003@gmail.com>
2022-03-25 17:21:40 +0300

    4

--- project/script.js
+++ project/script.js
@@ -2,7 +2,7 @@
 
 //alert(nn);
 
-function guess() {
+/*function guess() {
     var nn = prompt("Введите число");
     if (nn > 5) {
         alert("Число должно быть меньше");
@@ -16,5 +16,28 @@ function guess() {
     }
 }
 
+guess(); */
 
-guess();
+function timer() {
+    alert("Ты здесь слишком долго");
+}
+setTimeout(timer, 3000);
+
+function valid(){
+    var name = document.getElementById("name").value;
+    var password = document.getElementById("password").value;
+    
+    var reg_name = /^[a-zа-яё]+$/i;
+    var reg_password = /^[a-z0-9]{5, 100}/i;
+    
+    if (reg_name.test(name) == false){
+        alert ("Ошибка в поле ФИО");
+    }
+    if (reg_password.test(password) == false){
+        alert ("Ошибка в поле пароль");
+    }
+}
+
+document.querySelector(".button").addEventListener("click", valid);
+
+
